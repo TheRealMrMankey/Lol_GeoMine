e768986ee4166601125f6031cec560e46e9c3c73
Your Name <86630125+TheRealMrMankey@users.noreply.github.com>
2022-10-19 17:20:38 +0100

    Lighting Done+ Movement tweaks + Upgrades +Death

--- Assets/Tiago_GeoMine/Scripts/PlayerController.cs
+++ Assets/Tiago_GeoMine/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
+using UnityEngine.Experimental.Rendering.Universal; // Not an error
+using TMPro;
 
 namespace Tiago_GeoMine
 {
@@ -16,11 +18,14 @@ namespace Tiago_GeoMine
         [Header("UI")]
 
         public GameObject inventory;
+        public GameObject deathScreen;
 
         public GameObject shop;
         public GameObject lab;
 
         public GameObject[] buttons;
+
+        public TextMeshProUGUI healthText;
 
         // Tilemap and Navigation Variables
 
@@ -28,10 +33,15 @@ namespace Tiago_GeoMine
         private Tilemap tilemap;
         private NavMeshSurface surface2D;
 
+        // Sprite
+        private SpriteRenderer sprite;
+        private float lastValue;
+
         // Player Variables
         [Space(10)]
         [Header("Player")]
 
+        private int maxHp;
         public int healthPoints;
 
         // Inventory Variables
@@ -43,6 +53,7 @@ namespace Tiago_GeoMine
         /// Upgradable      
         public int pickaxeLvl = 1;
         public int lanternLvl = 1;
+        public Light2D helmetLight; // Not an error
         public int armourLvl = 1;
 
         [Space(10)]
@@ -76,21 +87,60 @@ namespace Tiago_GeoMine
             surface2D.BuildNavMeshAsync();
             surface2D.UpdateNavMesh(surface2D.navMeshData);
 
+            // Sprite
+            sprite = this.gameObject.GetComponent<SpriteRenderer>();
+
             #endregion
 
             #region UI
 
             // UI
             inventory.SetActive(false);
+            deathScreen.SetActive(false);
+
+            #endregion
+
+            #region Upgrades
+
+            // Helmet Light Radius
+            helmetLight.pointLightOuterRadius = lanternLvl * 2;
+            helmetLight.gameObject.SetActive(false);
+
+            // Health
+            if (armourLvl == 1)
+                healthPoints = 100;
+            else if (armourLvl == 2)
+                healthPoints = 150;
+            else if (armourLvl == 3)
+                healthPoints = 200;
+
+            maxHp = healthPoints;
+            healthText.text = healthPoints.ToString();
 
             #endregion
         }
 
         void Update()
         {
-            #region Movement and Rock Destruction (Mouse) //Needs Tweaking
+            #region Movement and Rock Destruction (Mouse)    
 
-            // Mouse Version
+            // Sprite Flip       
+            if (agent.velocity.x < 0 && agent.isStopped == false)
+            {
+                sprite.flipX = false;
+                lastValue = agent.velocity.x;
+            }
+            else if(agent.velocity.x > 0 && agent.isStopped == false)
+            {
+                sprite.flipX = true;
+                lastValue = agent.velocity.x;
+            }
+
+            if (agent.isStopped && lastValue < 0)
+                sprite.flipX = true;
+            else if(agent.isStopped && lastValue > 0)
+                sprite.flipX = false;
+
             if (Input.GetMouseButtonDown(0))
             {
                 if (Input.GetMouseButtonDown(0))
@@ -103,10 +153,8 @@ namespace Tiago_GeoMine
 
                     if (hit.collider != null)
                     {
-                        // Test
-                        Debug.Log("Hit: " + hit.transform.name);
-
                         // Movement
+
                         /// Buildings
                         if (hit.transform.tag == "Shop")
                             GoToShop();
@@ -122,7 +170,7 @@ namespace Tiago_GeoMine
                             Vector3 offset = new Vector3(.5f, .35f, 0);
 
                             // Check if it is possible for the player to reach
-                            NavMeshPath navPath = new NavMeshPath();
+                            NavMeshPath navPath = new NavMeshPath();                        
 
                             if (agent.CalculatePath(mousePos2D, navPath) && navPath.status == NavMeshPathStatus.PathComplete)
                                 agent.SetDestination(offset + tilePos);
@@ -133,6 +181,17 @@ namespace Tiago_GeoMine
                             Vector3 tilePos = tilemap.WorldToCell(mousePos);
                             Vector3 offset = new Vector3(.5f, .35f, 0);
                             agent.SetDestination(offset + tilePos);
+                        }
+
+                        // Attacking Enemy
+                        if (hit.transform.tag == "Enemy")
+                        {
+                            // Check if it is in range
+                            float distance = Vector3.Distance(transform.position, hit.transform.position);
+
+                            // Kill enemy
+                            if (distance < 1.75f)
+                                Destroy(hit.transform.gameObject);
                         }
 
                         // Mining
@@ -147,11 +206,24 @@ namespace Tiago_GeoMine
                                 TileBase tileBase = tilemap.GetTile(tilemap.WorldToCell(mousePos));
                                 string tileName = tileBase.ToString();
 
-                                // Find out which rock was destroyed
-                                Destroy(tileName);
-                                
-                                // Destroy Tile
-                                tilemap.SetTile(tilemap.WorldToCell(mousePos), null);                            
+                                // If pickaxe is at least level 1, mine
+                                if (tileName.Contains("Calcium") | tileName.Contains("Silicon") | tileName.Contains("Aluminium") | tileName.Contains("Iron") && pickaxeLvl >= 1)
+                                {
+                                    // Find out which rock was destroyed
+                                    Destroy(tileName);
+
+                                    // Destroy Tile
+                                    tilemap.SetTile(tilemap.WorldToCell(mousePos), null);
+                                }
+                                // If pickaxe if level 2, mine
+                                else if(tileName.Contains("Sedimentary") | tileName.Contains("Igneous") | tileName.Contains("Metamorphic") && pickaxeLvl == 2)
+                                {
+                                    // Find out which rock was destroyed
+                                    Destroy(tileName);
+
+                                    // Destroy Tile
+                                    tilemap.SetTile(tilemap.WorldToCell(mousePos), null);
+                                }
 
                                 // Update Walkable Area
                                 surface2D.UpdateNavMesh(surface2D.navMeshData);
@@ -163,20 +235,42 @@ namespace Tiago_GeoMine
 
             #endregion
 
+            #region Lantern Upgrade Switch place to shop later
+
+            helmetLight.pointLightOuterRadius = lanternLvl * 2;
+
+            #endregion
+
             #region Death
 
             // Death
             if (healthPoints <= 0)
             {
-                //Teleport to starting position
+                //Regain all health
+                healthPoints = maxHp;
+
+                // Respawn
+                /// Teleport to starting position
                 agent.Warp(new Vector2(-2.9f, 0.57f));
 
-                //Regain all health
-                healthPoints = 100;             
+                agent.isStopped = true;
+                agent.destination = agent.transform.position;
+                deathScreen.SetActive(true);
             }
 
             #endregion
         }
+
+        #region Death Screen
+
+        public void CloseDeathScreen()
+        {
+            deathScreen.SetActive(false);
+            agent.isStopped = false;
+            agent.destination = agent.transform.position;
+        }
+
+        #endregion
 
         #region Type of Rock
 
@@ -204,7 +298,7 @@ namespace Tiago_GeoMine
 
         public void GoToEntrance()
         {
-            agent.SetDestination(new Vector2(-7.83f, 0.34f));
+            agent.SetDestination(new Vector2(-7.448f, 0.34f));
         }
 
         public void GoToShop()
@@ -226,6 +320,9 @@ namespace Tiago_GeoMine
         {
             if (inventory.activeSelf == false) // Open
             {
+                agent.isStopped = true;
+                agent.destination = agent.transform.position;
+
                 inventory.SetActive(true);
 
                 // Update Backpack
@@ -240,6 +337,9 @@ namespace Tiago_GeoMine
             }
             else // Close
             {
+                agent.isStopped = false;
+                agent.destination = agent.transform.position;
+
                 // Enable buttons
                 for (int i = 0; i < buttons.Length; i++)
                 {
@@ -258,7 +358,24 @@ namespace Tiago_GeoMine
         private void OnCollisionEnter2D(Collision2D collision)
         {
             if (collision.transform.tag == "Enemy")
+            {
                 healthPoints -= 10;
+                healthText.text = healthPoints.ToString();
+            }          
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.transform.tag == "Trigger")
+            {
+                Debug.Log("trigger");
+
+                if(helmetLight.gameObject.activeSelf == false)
+                    helmetLight.gameObject.SetActive(true);
+                else
+                    helmetLight.gameObject.SetActive(false);
+
+            }
         }
 
         #endregion
